795e5f8d60c839dac4e66ed59a87d95fd2d763ef
Nate Gates <nate.gates@daktronics.com>
2019-04-17 09:02:32 -0500

    updated packages to use latest of everyting switched to yarn, evertyhing seems to work.

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,23 @@ var ts = require("gulp-typescript");
 
 
 var tsBuildOpts = {
-	//"module": "amd",
-	"target": "es5",
-	"noEmitOnError": true,
-	"removeComments":true,
+    //"module": "amd",
+    "target": "es5",
+    "noEmitOnError": true,
+    "removeComments": true,
 };
 
-gulp.task("default",function(){
-   
-   //var srcs =["./*.ts", "./js/*.ts"];
- 
-  	gulp.src("./*.ts")
-		.pipe(ts(tsBuildOpts))
-		.pipe(gulp.dest("./"));
-    
-	gulp.src("./js/*.ts")
-		.pipe(ts(tsBuildOpts))
-		.pipe(gulp.dest("./js/"));    
+gulp.task("default", function (cb) {
+
+    //var srcs =["./*.ts", "./js/*.ts"];
+
+    gulp.src("./*.ts")
+        .pipe(ts(tsBuildOpts))
+        .pipe(gulp.dest("./"));
+
+    gulp.src("./js/*.ts")
+        .pipe(ts(tsBuildOpts))
+        .pipe(gulp.dest("./js/"));
+
+    cb();
 });
