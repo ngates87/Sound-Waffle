0f4f1f5718f27f600c9912b8fb619e556f4e6a01
Nate Gates <nate.gates@daktronics.com>
2021-05-18 20:17:04 -0500

    update angular/cdk to latest

--- v2/soundwafle/src/app/playlist-cue/playlist-cue.component.spec.ts
+++ v2/soundwafle/src/app/playlist-cue/playlist-cue.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlaylistCueComponent } from './playlist-cue.component';
 
@@ -6,7 +6,7 @@ describe('PlaylistCueComponent', () => {
   let component: PlaylistCueComponent;
   let fixture: ComponentFixture<PlaylistCueComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlaylistCueComponent ]
     })
